b64f7a9fe9bac24949baef9439805521166f11f1
goncaloazevedoq <goncalooazevedo@outlook.com>
2021-11-24 21:21:21 +0000

    Fix typo

--- classes.py
+++ classes.py
@@ -13,7 +13,7 @@ class Card:
 
 class Deck:
     def __init__(self):
-        self.cards = [Card(a, b) for a in ['2', '3', '4', '5',' 6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'] for b in ['Hearts', 'Spades', 'Clubs', 'Diamonds']]
+        self.cards = [Card(a, b) for a in ['2', '3', '4', '5','6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'] for b in ['Hearts', 'Spades', 'Clubs', 'Diamonds']]
         
     def shuffle(self):
         random.shuffle(self.cards)
